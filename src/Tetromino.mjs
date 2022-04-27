3b2b9ff72754ced623f5b843ff5c0f0a54da6db0
Jan Rundt <jan.rundt@gmail.com>
2022-04-27 15:01:49 +0300

    second tetromino

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -4,5 +4,6 @@ export class Tetromino {
 
   static T_SHAPE = new RotatingShape('.T.TTT...');
 
+  static I_SHAPE = new RotatingShape('..........IIII...........')
 
 }
--- test/RotatingTetrominoes.test.mjs
+++ test/RotatingTetrominoes.test.mjs
@@ -48,7 +48,7 @@ describe("The T shape", () => {
 });
 
 
-/*
+
 describe("The I shape", () => {
   const shape = Tetromino.I_SHAPE;
 
@@ -86,7 +86,7 @@ describe("The I shape", () => {
     expect(distinctOrientations(shape).size).to.equal(2);
   });
 });
-*/
+
 
 /*
 describe("The O shape", () => {
