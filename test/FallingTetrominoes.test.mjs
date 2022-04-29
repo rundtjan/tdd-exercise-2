bad79b6bcfec7038d2642859c61ce8cac14a6aae
Jan Rundt <jan.rundt@gmail.com>
2022-04-29 10:07:51 +0300

    3rd test with new T shape

--- test/FallingTetrominoes.test.mjs
+++ test/FallingTetrominoes.test.mjs
@@ -55,18 +55,18 @@ describe("Falling tetrominoes", () => {
   });
 
   it("stop when they land on another block", () => {
-    board.drop(Tetromino.T_SHAPE);
+    board.drop(Tetromino.T_SHAPE2);
     fallToBottom(board);
-    board.drop(Tetromino.T_SHAPE);
+    board.drop(Tetromino.T_SHAPE2);
     fallToBottom(board);
 
     expect(board.toString()).to.equalShape(
       `..........
        ..........
-       ....T.....
        ...TTT....
        ....T.....
-       ...TTT....`
+       ...TTT....
+       ....T.....`
     );
   });
 });
