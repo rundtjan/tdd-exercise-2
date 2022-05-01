456720ff5539a2bf87d826e50cf1466ff38aa800
Jan Rundt <jan.rundt@gmail.com>
2022-05-01 11:40:01 +0300

    refactor: delete old tetrominoes

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -9,13 +9,7 @@ import { J_shape } from '../src/tetrominoes/J_shape.mjs';
 
 export class Tetromino {
 
-  //static T_SHAPE = new RotatingShape('.T.TTT...');
-
   static T_SHAPE2 = new T_shape(0);
-
-  //static I_SHAPE = new RotatingShape('..........IIII...........')
-
-  static O_SHAPE = new RotatingShape('.OO.OO...')
 
   static Z_SHAPE = new Z_shape(0);
 
